ae07db903f357d2e34eb091ab4241f2459a7e94d
Harry <otgo@outlook.es>
2019-06-14 04:28:38 -0500

    Update captcha.c

--- captcha.c
+++ captcha.c
@@ -1,3 +1,7 @@
+// if we're using g++ compiler
+#ifdef __cplusplus
+    extern "C" {
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,16 +12,43 @@
 #include <time.h>
 #include "captcha.h"
 static int generate(lua_State* L) {
-    const char *output_file = luaL_checkstring(L, 1);
-    int size_letters = luaL_checknumber(L, 2);
-    int font_size = luaL_checknumber(L, 3);
+    const char *usr_path=NULL, *usr_fmt = NULL; // set user path and user format to NULL
+    int usr_fntsize = 0, usr_length = 0; // set sizes to 0 (false)
+    luaL_checktype(L, 1, LUA_TTABLE); // check if arg1 is a table
+    luaL_getmetatable(L, "CAPTCHA"); // get metatable
+    lua_pushnil(L);               // Push nil
+    while(lua_next(L, 1) != 0) { // goes element by element in metatable
+        const char *var = lua_tostring(L, -2); // get variable metatable
+        const char *val = lua_tostring(L, -1); // get value metatable
+        if(!strcmp(var, "__path")) usr_path = val;
+        if(!strcmp(var, "__format")) usr_fmt = val;
+        else if(!strcmp(var, "__length")) usr_length = atoi(val);
+        else if(!strcmp(var, "__fntsize")) usr_fntsize = atoi(val); // set usr settings
+        lua_pop(L, 1); // Pop value
+    }
+    lua_pop(L, 1); // pop tab, continue code
+    if (usr_path==NULL) {
+        usr_path = "./"; // default path
+    }
+    if (usr_length==0) {
+        usr_length = 5; // default length
+    }
+    if (usr_fntsize==0) {
+        usr_fntsize = 60; // default font size
+    }
+    if (usr_fmt==NULL) {
+        usr_fmt = "png"; // default format
+    }
+    char *path = (char*)lua_newuserdata(L, strlen(usr_path)+usr_length); // works like malloc, adding dat to gc
+    lua_pop(L, 1); // continues
+    sprintf(path, "%s/", usr_path);
     FILE *out;
-    int x_image = font_size*size_letters;
-    int y_image = font_size*2;
-    char letter[2];
+    int x_image = usr_fntsize*usr_length; // calcule width image, using length * font size
+    int y_image = usr_fntsize*2; // calcule height image, using font size * 2
+    char letter[2]; // set letters string with 2 bytes
     int i, y, x;
     srand(time(NULL)); /* starts srand with os time */
-    gdImagePtr im = gdImageCreateTrueColor (x_image, y_image);
+    gdImagePtr im = gdImageCreateTrueColor (x_image, y_image); // creates image
     /* Background color for gfImageCreateTrueColor */
     gdImageFilledRectangle (im, 0, 0, gdImageSX (im), gdImageSY (im), gdImageColorAllocate(im, 255, 255, 255));
     /* gdImageCreate, will be first allocated */
@@ -29,42 +60,100 @@ static int generate(lua_State* L) {
     };
     y = y_image/2; // set first letter in the center
     x = 0; // set first letter at the start (0xY)
-    int random_color = gdImageColorAllocate(im, rand()%200, rand()%200, rand()%200);
-    for (i = 0; i<=size_letters; i++) {
+    int random_color = gdImageColorAllocate(im, rand()%200, rand()%200, rand()%200); // generate a random color, used by text
+    for (i = 0; i<=usr_length; i++) {
         char rdm_letter = troc(t_w[rand()%48]);
+        sprintf(path, "%s%c", path, rdm_letter);
         sprintf(letter, "%c", rdm_letter);
         char *error = gdImageStringTTF(
             im, // im	The image to draw onto.
             NULL, //brect	The bounding rectangle as array of 8 integers where each pair represents the x- and y-coordinate of a point.  The points specify the lower left, lower right, upper right and upper left corner.
             random_color, // fg	The font color
             fonts[rand()%12], // fontlist	The semicolon delimited list of font filenames to look for.
-            font_size, // ptsize	The height of the font in typographical points (pt).
+            usr_fntsize, // ptsize	The height of the font in typographical points (pt).
             rand()%1, // angle	The angle in radian to rotate the font counter-clockwise.
             x, // x	The x-coordinate of the basepoint (roughly the lower left corner) of the first letter.
             rand()%y+y_image/2, // y	The y-coordinate of the basepoint (roughly the lower left corner) of the first letter.
             letter // string	The string to render.
         );
-        x+=font_size;
-        if(error) printf("Couldn't create image string. %s\n", error);
+        x+=usr_fntsize;
     }
     for (i=0; i<(y_image*x_image)/2; i++) {
         int random_x = rand()%x_image;
         int random_y = rand()%y_image;
-        gdImageSetPixel(im, random_x, random_y, random_color-4000);
+        gdImageSetPixel(im, random_x, random_y, random_color-4000); // generate another "random" color subtracting 4000
     }
-    out = fopen (output_file, "wb");
+    sprintf(path, "%s.%s", path, usr_fmt);
+    out = fopen (path, "wb");
     gdImagePng (im, out);
     fclose (out);
     gdImageDestroy(im);
+    lua_pushstring(L, path);
     return 1;
 }
-
-static const struct luaL_Reg captcha[] = {
-    {"generate", generate},
-    {NULL, NULL}
-};
-int luaopen_captcha(lua_State* L) { // returns library in table: library.function, must be luapen_libname
-    luaL_newlibtable(L, captcha); // adds "x" struct (those are the commands), only creates the table
-    luaL_setfuncs(L, captcha, 0); // im not sure what does mean '0', i think 0 means start of struct, not sure.
+static int setlength(lua_State* L) {
+    lua_Number xsize = luaL_checknumber(L, 2);
+    lua_pushnumber(L, xsize);
+	lua_setfield(L, -3, "__length"); // works by gettings mettatable (-3)
+    luaL_getmetatable(L, "CAPTCHA");
+    lua_setmetatable(L, -2);
     return 1;
 }
+static int setfontsize(lua_State* L) {
+    lua_Number xsize = luaL_checknumber(L, 2);
+    lua_pushnumber(L, xsize);
+	lua_setfield(L, -3, "__fntsize"); // works by gettings mettatable (-3)
+    luaL_getmetatable(L, "CAPTCHA");
+    lua_setmetatable(L, -2);
+    return 1;
+}
+static int setpath(lua_State* L) {
+    const char *path = luaL_checkstring(L, 2);
+    lua_pushstring(L, path);
+	lua_setfield(L, -3, "__path"); // works by gettings mettatable (-3)
+    luaL_getmetatable(L, "CAPTCHA");
+    lua_setmetatable(L, -2);
+    return 1;
+}
+static int setformat(lua_State* L) {
+    const char *format = luaL_checkstring(L, 2);
+    lua_pushstring(L, format);
+	lua_setfield(L, -3, "__format"); // works by gettings mettatable (-3)
+    luaL_getmetatable(L, "CAPTCHA");
+    lua_setmetatable(L, -2);
+    return 1;
+}
+static int L_new(lua_State *L) {
+    static const struct luaL_Reg L_methods[] = {
+        {"generate", &generate},
+        {"setlength", &setlength},
+        {"setfontsize", &setfontsize},
+        {"setpath", &setpath},
+        {"setformat", &setformat},
+        {NULL, NULL}
+    };
+    luaL_newmetatable(L, "CAPTCHA");
+    lua_setmetatable(L, -1);
+    luaL_newlibtable(L, L_methods); // adds struct (those are the commands), only creates the table
+    luaL_setfuncs(L, L_methods, 0);
+    lua_pushvalue(L, -1); // push the metatable
+    lua_setfield(L, -1,"__index");
+    lua_pushstring(L, "1.9");
+	lua_setfield(L, -2, "VERSION");
+    //printf("%i\n", LUA_VERSION_NUM );
+    return 1;
+}
+int luaopen_captcha(lua_State* L) {
+    static const struct luaL_Reg L_register[] = {
+        {"new", &L_new},
+        {NULL, NULL}
+    };
+    luaL_newlib(L, L_register);
+    luaL_setfuncs(L, L_register, 0);
+    lua_pushstring(L, "1.9");
+	lua_setfield(L, -2, "VERSION");
+    return 1;
+}
+#ifdef __cplusplus
+}
+#endif
